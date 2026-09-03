7581a5e5e2b9ded20bb78131d1fd6d473b370dee
JAGATH JYOTHIS T S <jagathjyothis2005@gmail.com>
2026-09-03 22:11:21 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a= ""
+        for i in s:
+            if i.isalnum():
+                a = a + i.lower()
+        return a == a[::-1]
